6581a9aa49bc9286f933344c48c9e4b1e8489cec
Caio Guimarães <caio23guimaraes@gmail.com>
2022-02-01 00:45:34 -0300

    Adicionando produção para todos os arquivos gerados

--- gera.py
+++ gera.py
@@ -2,15 +2,15 @@ import random
 
 entrada = open('entrada.in', 'w')
 
-for i in range (6):
+for i in range (50):
     nome_arq = 'AxB_' + str(i+1)
     entrada.write(nome_arq + '\n') # Adiciona o nome dos arquivos de cada matriz ao arquivo entrada.in
 
     arq = open(nome_arq, 'w') # Cria os arquivos com suas respectivas matrizes
 
     # Cria e escreve a primeira matriz
-    for m in range(2):
-        for n in range(2):
+    for m in range(10):
+        for n in range(10):
             num = random.uniform(-10,+10)
             arq.write('{:.2f}'.format(num) + ' ')
         arq.write(str('\n'))
@@ -18,8 +18,8 @@ for i in range (6):
     arq.write(str('\n')) # Linha de separação entre as matrizes
 
     # Cria e escreve a segunda matriz
-    for m in range(2):
-        for n in range(2):
+    for m in range(10):
+        for n in range(10):
             num = random.uniform(-10,+10)
             arq.write('{:.2f}'.format(num) + ' ')
         arq.write(str('\n'))
--- projeto.c
+++ projeto.c
@@ -4,11 +4,13 @@
 #include <pthread.h>
 #include </usr/include/semaphore.h>
 
-#define BUFF_SIZE   5		/* número total de arquivos abertos por vez */
-#define NP          1		/* número de produtores*/
-#define NCP         3		/* número de consumidores/produtores  */
-#define NC          1		/* número de consumidores */
-#define M_SIZE      2  /* ordem da matriz */
+#define BUFF_SIZE   5       /* tamanho do buffer */
+#define NP          1       /* número de produtores*/
+#define NCP1         5      /* número de consumidores/produtores  */
+#define NCP2         4		  /* número de consumidores/produtores  */
+#define NCP3         3		  /* número de consumidores/produtores  */
+#define NC          1       /* número de consumidores */
+#define M_SIZE      10       /* ordem da matriz */
 
 typedef struct S{
   char *nome;
@@ -29,22 +31,22 @@ typedef struct {
 pc_buf shared[4]; // cria 4 regiões de memória compartilhadas
 
 void imprime_matriz(double **m){
-  for(int i=0; i<2; i++){
-    for(int j=0; j<2; j++)
+  for(int i=0; i<M_SIZE; i++){
+    for(int j=0; j<M_SIZE; j++)
       printf("%.2lf ", m[i][j]);
     printf("\n");
   }
 }
 
 void imprime_vetor(double *v){
-  for(int i=0; i<2; i++)
+  for(int i=0; i<M_SIZE; i++)
     printf("%.2lf ", v[i]);
   printf("\n");
 }
 
 void escreve_matriz(double **m, FILE *arq){
-  for(int i=0; i<2; i++){
-    for(int j=0; j<2; j++){
+  for(int i=0; i<M_SIZE; i++){
+    for(int j=0; j<M_SIZE; j++){
       fprintf(arq, "%.2lf ", m[i][j]);
     }
     fprintf(arq, "\n");
@@ -52,7 +54,7 @@ void escreve_matriz(double **m, FILE *arq){
 }
 
 void escreve_vetor(double *v, FILE *arq){
-  for(int i=0; i<2; i++)
+  for(int i=0; i<M_SIZE; i++)
     fprintf(arq, "%.2lf ", v[i]);
   fprintf(arq, "\n");
 }
@@ -70,17 +72,23 @@ void *Produtor(void *arg){
     exit(-1);
   }
 
-  while(fscanf(entrada, "%s", nome_arq) == 1){ // se conseguiu fazer a leitura da próxima linha
+  // continua loop se conseguiu fazer a leitura da próxima linha
+  while(fscanf(entrada, "%s", nome_arq) == 1){
     arq = fopen(nome_arq, "r");
-    // printf("Arquivo: %s\n", nome_arq);
 
     sem_wait(&shared[0].empty);
     sem_wait(&shared[0].mutex);
 
+    /*
+      Alocação de espaço na memória para as matrizes A, B, C
+      para o vetor V e para o nome do arquivo que contém as matrizes
+    */
     struct S *sP = (struct S*)malloc(sizeof(struct S));
     sP->a = (double **)malloc(sizeof(double *) * M_SIZE);
     sP->b = (double **)malloc(sizeof(double *) * M_SIZE);
     sP->c = (double **)malloc(sizeof(double *) * M_SIZE);
+    sP->v = (double *)malloc(sizeof(double *)*M_SIZE);
+    sP->nome = (char *)malloc(sizeof(char *)*25);
 
     for(int i=0; i<M_SIZE; i++){
       sP->a[i] = (double *)malloc(sizeof (double)*M_SIZE);
@@ -88,30 +96,30 @@ void *Produtor(void *arg){
       sP->c[i] = (double *)malloc(sizeof (double)*M_SIZE);
     }
 
-    sP->nome = (char *)malloc(sizeof(char *)*25);
+    /* Cópia do contéudo do nome do arquivo para a estrutura compartilhada sP e
+      inicialização do valor E
+    */
     strcpy(sP->nome, nome_arq);
+    sP->e = 0;
 
+    // Leitura Matriz A
     for(int i=0; i<M_SIZE; i++){
       for(int j=0; j<M_SIZE; j++){
         fscanf(arq, "%lf ", &sP->a[i][j]);
       }
     }
 
-    // printf("Produz matriz A do arq: %s\n", sP->nome);
-    // imprime_matriz(sP->a);
-    // printf("\n");
-    // fflush(stdout);
-
+    // Leitura Matriz B
     for(int i=0; i<M_SIZE; i++){
       for(int j=0; j<M_SIZE; j++){
         fscanf(arq, "%lf ", &sP->b[i][j]);
       }
     }
 
-    // printf("Produz matriz B do arq: %s\n", sP->nome);
+    printf("\nProdutor - Produzindo arq: %s\n", sP->nome);
+    // imprime_matriz(sP->a);
     // imprime_matriz(sP->b);
-    // printf("\n###########################\n\n");
-    // fflush(stdout);
+    fflush(stdout);
 
     shared[0].buffer[shared[0].in] = sP;
     shared[0].in = (shared[0].in+1)%BUFF_SIZE;
@@ -126,181 +134,202 @@ void *Produtor(void *arg){
 }
 
 void *CP1(void *arg){
-  // int id = *((int *)arg);
-  // printf("Thread ID: %d\n", id);
-  // Consumindo elementos da região shared[0]
-  sem_wait(&shared[0].full);
-  sem_wait(&shared[0].mutex);
+  int id = (int)arg;
 
-  struct S *sP;
-  sP = shared[0].buffer[shared[0].out];
-  shared[0].out = (shared[0].out+1)%BUFF_SIZE;
+  // Porção de vezes que a thread deve executar
+  int p = 50/NCP1;
+  int r = 50%NCP1;
+  int lim = (id+1)*p;
 
-  // printf("\n###########################\n\n");
-  // printf("\nCP1: Matriz A do arq: %s\n", sP->nome);
-  // imprime_matriz(sP->a);
-  // fflush(stdout);
-  //
-  // printf("\nMatriz B\n");
-  // imprime_matriz(sP->b);
-  // fflush(stdout);
+  if(id==NCP1-1)
+    lim = lim+r;
 
-  // Multiplicação das matrizes
-  for(int i=0; i<M_SIZE; i++){
-    for(int j=0; j<M_SIZE; j++){
-      sP->c[i][j] = 0;
-      for(int k=0; k<M_SIZE; k++)
-        sP->c[i][j] += sP->a[i][k]*sP->b[k][j];
+  for(int i=id*p; i<lim; i++){
+    // Consumindo elementos da região shared[0]
+    sem_wait(&shared[0].full);
+    sem_wait(&shared[0].mutex);
+
+    struct S *sP;
+    sP = shared[0].buffer[shared[0].out];
+    shared[0].out = (shared[0].out+1)%BUFF_SIZE;
+
+    printf("\nCP1 - Consumindo arq: %s\n", sP->nome);
+    // imprime_matriz(sP->a);
+    // imprime_matriz(sP->b);
+    fflush(stdout);
+
+    // Multiplicação das matrizes
+    for(int i=0; i<M_SIZE; i++){
+      for(int j=0; j<M_SIZE; j++){
+        sP->c[i][j] = 0;
+        for(int k=0; k<M_SIZE; k++)
+          sP->c[i][j] += sP->a[i][k]*sP->b[k][j];
+      }
     }
+
+    printf("\nCP1 - Produzindo arq: %s\n", sP->nome);
+    // imprime_matriz(sP->c);
+    fflush(stdout);
+
+    shared[2].buffer[shared[2].in] = sP;
+    shared[2].in = (shared[2].in+1)%BUFF_SIZE;
+
+    sem_post(&shared[0].mutex);
+    sem_post(&shared[0].empty);
+
+    // Produzindo elementos para a região shared[1]
+    sem_wait(&shared[1].empty);
+    sem_wait(&shared[1].mutex);
+
+    shared[1].buffer[shared[1].in] = sP;
+    shared[1].in = (shared[1].in+1)%BUFF_SIZE;
+
+    sem_post(&shared[1].mutex);
+    sem_post(&shared[1].full);
   }
-  // printf("\nMatriz C\n");
-  // imprime_matriz(sP->c);
-  // printf("\n###########################\n\n");
-  // fflush(stdout);
-
-  shared[2].buffer[shared[2].in] = sP;
-  shared[2].in = (shared[2].in+1)%BUFF_SIZE;
-
-  sem_post(&shared[0].mutex);
-  sem_post(&shared[0].empty);
-
-  // Produzindo para a região shared[1]
-  sem_wait(&shared[1].empty);
-  sem_wait(&shared[1].mutex);
-
-  shared[1].buffer[shared[1].in] = sP;
-  shared[1].in = (shared[1].in+1)%BUFF_SIZE;
-
-  sem_post(&shared[1].mutex);
-  sem_post(&shared[1].full);
-
 }
 
 void *CP2(void *arg){
-  // Consumindo elementos da região shared[1]
-sem_wait(&shared[1].full);
-sem_wait(&shared[1].mutex);
+  int id = (int)arg;
 
-struct S *sP;
-sP = shared[1].buffer[shared[1].out];
-shared[1].out = (shared[1].out+1)%BUFF_SIZE;
+  // Porção de vezes que a thread deve executar
+  int p = 50/NCP2;
+  int r = 50%NCP2;
+  int lim = (id+1)*p;
 
-// printf("\nCP2: Matriz C do arq: %s\n", sP->nome);
-// imprime_matriz(sP->c);
-// // printf("\n###########################\n\n");
-// fflush(stdout);
+  if(id==NCP2-1)
+    lim = lim+r;
 
-sem_post(&shared[1].mutex);
-sem_post(&shared[1].empty);
+  for(int i=id*p; i<lim; i++){
+    // Consumindo elementos da região shared[1]
+    sem_wait(&shared[1].full);
+    sem_wait(&shared[1].mutex);
 
-// Produzindo para a região shared[2]
-sem_wait(&shared[2].empty);
-sem_wait(&shared[2].mutex);
+    struct S *sP;
+    sP = shared[1].buffer[shared[1].out];
+    shared[1].out = (shared[1].out+1)%BUFF_SIZE;
 
-sP->v = (double *)malloc(sizeof(double *)*10);
+    printf("\nCP2 - Consumindo arq: %s\n", sP->nome);
+    // imprime_matriz(sP->c);
+    fflush(stdout);
 
-// printf("\nCP2: soma coluna para Arq: %s\n", sP->nome);
-for(int i=0; i<M_SIZE; i++){
-  sP->v[i] = 0;
-  for(int j=0; j<M_SIZE; j++){
-    sP->v[i] += sP->c[j][i];
+    sem_post(&shared[1].mutex);
+    sem_post(&shared[1].empty);
+
+    // Produzindo elementos para a região shared[2]
+    sem_wait(&shared[2].empty);
+    sem_wait(&shared[2].mutex);
+
+    // Soma das colunas da Matriz C
+    for(int i=0; i<M_SIZE; i++){
+      sP->v[i] = 0;
+      for(int j=0; j<M_SIZE; j++){
+        sP->v[i] += sP->c[j][i];
+      }
+    }
+
+    printf("\nCP2 - Produzindo arq: %s\n", sP->nome);
+    // imprime_vetor(sP->v);
+    fflush(stdout);
+
+    shared[2].buffer[shared[2].in] = sP;
+    shared[2].in = (shared[2].in+1)%BUFF_SIZE;
+
+    sem_post(&shared[2].mutex);
+    sem_post(&shared[2].full);
   }
-  // printf("%lf ", sP->v[i]);
-}
-// printf("\n###########################\n\n");
-// fflush(stdout);
-
-shared[2].buffer[shared[2].in] = sP;
-shared[2].in = (shared[2].in+1)%BUFF_SIZE;
-
-sem_post(&shared[2].mutex);
-sem_post(&shared[2].full);
 }
 
 void *CP3(void *arg){
-  // Consumindo elementos da região shared[2]
-  sem_wait(&shared[2].full);
-  sem_wait(&shared[2].mutex);
+  int id = (int)arg;
 
-  struct S *sP;
-  sP = shared[2].buffer[shared[2].out];
-  shared[2].out = (shared[2].out+1)%BUFF_SIZE;
+  // Porção de vezes que a thread deve executar
+  int p = 50/NCP3;
+  int r = 50%NCP3;
+  int lim = (id+1)*p;
 
-  printf("CP3: soma da coluna para Arq: %s\n", sP->nome);
-  for(int i=0; i<M_SIZE; i++){
-    printf("%lf ", sP->v[i]);
+  if(id==NCP3-1)
+    lim = lim+r;
+
+  for(int i=id*p; i<lim; i++){
+    // Consumindo elementos da região shared[2]
+    sem_wait(&shared[2].full);
+    sem_wait(&shared[2].mutex);
+
+    struct S *sP;
+    sP = shared[2].buffer[shared[2].out];
+    shared[2].out = (shared[2].out+1)%BUFF_SIZE;
+
+    printf("\nCP3 - Consumindo arq: %s\n", sP->nome);
+    fflush(stdout);
+
+    sem_post(&shared[2].mutex);
+    sem_post(&shared[2].empty);
+
+    // Produzindo elementos para a região shared[3]
+    sem_wait(&shared[3].empty);
+    sem_wait(&shared[3].mutex);
+
+    // Soma dos valores do vetor V armazenada em E
+    for(int i=0; i<M_SIZE; i++)
+      sP->e += sP->v[i];
+
+    printf("\nCP3 - Produzindo arq: %s\n", sP->nome);
+    fflush(stdout);
+
+    shared[3].buffer[shared[3].in] = sP;
+    shared[3].in = (shared[3].in+1)%BUFF_SIZE;
+
+    sem_post(&shared[3].mutex);
+    sem_post(&shared[3].full);
   }
-  // printf("\n###########################\n\n");
-  fflush(stdout);
-
-  sem_post(&shared[2].mutex);
-  sem_post(&shared[2].empty);
-
-  // Produzindo para a região shared[3]
-  sem_wait(&shared[3].empty);
-  sem_wait(&shared[3].mutex);
-
-  // sP->e = (double *)malloc(sizeof(double *));
-  sP->e = 0;
-  for(int i=0; i<M_SIZE; i++){
-    sP->e += sP->v[i];
-  }
-  printf("\nCP3: soma dos elementos de v para Arq: %s\n", sP->nome);
-  printf("%lf\n", sP->e);
-  printf("\n###########################\n\n");
-  fflush(stdout);
-
-  shared[3].buffer[shared[3].in] = sP;
-  shared[3].in = (shared[3].in+1)%BUFF_SIZE;
-
-  sem_post(&shared[3].mutex);
-  sem_post(&shared[3].full);
 }
 
 void *Consumidor(void *arg){
-  // Consumindo elementos da região shared[3]
-  sem_wait(&shared[3].full);
-  sem_wait(&shared[3].mutex);
-  //
-  struct S *sP;
-  sP = shared[3].buffer[shared[3].out];
-  shared[3].out = (shared[3].out+1)%BUFF_SIZE;
-
-  printf("\nConsumidor: soma dos elementos para Arq: %s\n", sP->nome);
-  printf("%lf ", sP->e);
-  printf("\n###########################\n\n");
-  fflush(stdout);
-
   FILE *saida;
   saida = fopen("saida.out", "w");
 
-  fprintf(saida, "####### %s #######\n", sP->nome);
-  fprintf(saida, "Matriz A\n");
-  escreve_matriz(sP->a, saida);
+  for(int i=0; i<50; i++){
+    // Consumindo elementos da região shared[3]
+    sem_wait(&shared[3].full);
+    sem_wait(&shared[3].mutex);
 
-  fprintf(saida, "\nMatriz B\n");
-  escreve_matriz(sP->b, saida);
+    struct S *sP;
+    sP = shared[3].buffer[shared[3].out];
+    shared[3].out = (shared[3].out+1)%BUFF_SIZE;
 
-  fprintf(saida, "\nMatriz C\n");
-  escreve_matriz(sP->c, saida);
+    printf("\nConsumidor - Consumindo arq %s\n", sP->nome);
+    // printf("%lf ", sP->e);
+    // printf("\n###########################\n\n");
+    fflush(stdout);
 
-  fprintf(saida, "\nVetor V\n");
-  escreve_vetor(sP->v, saida);-
+    // Construção do arquivo de saída
+    fprintf(saida, "####### %s #######\n", sP->nome);
+    fprintf(saida, "Matriz A\n");
+    escreve_matriz(sP->a, saida);
 
-  fprintf(saida, "\nE\n");
-  fprintf(saida, "%.2lf\n", sP->e);
+    fprintf(saida, "\nMatriz B\n");
+    escreve_matriz(sP->b, saida);
 
-  fprintf(saida, "\n");
+    fprintf(saida, "\nMatriz C\n");
+    escreve_matriz(sP->c, saida);
 
+    fprintf(saida, "\nVetor V\n");
+    escreve_vetor(sP->v, saida);-
+
+    fprintf(saida, "\nE\n");
+    fprintf(saida, "%.2lf\n", sP->e);
+
+    fprintf(saida, "\n");
+
+    sem_post(&shared[3].mutex);
+    sem_post(&shared[3].empty);
+  }
   fclose(saida);
-
-  sem_post(&shared[3].mutex);
-  sem_post(&shared[3].empty);
 }
 
 int main(int argc, char const *argv[]) {
-
-  pthread_t idP, idC, idCP1[5], idCP2[4], idCP3[3];
+  pthread_t idP, idC, idCP1[NCP1], idCP2[NCP2], idCP3[NCP3];
 
   for(int i=0; i<4; i++){
     sem_init(&shared[i].full, 0, 0);
@@ -308,31 +337,27 @@ int main(int argc, char const *argv[]) {
     sem_init(&shared[i].mutex, 0, 1);
   }
 
+  // Criando instâncias do Produtor -  Recebe o nome do arquivo de entrada como argumento
   pthread_create(&idP, NULL, Produtor, (void *)argv[1]);
-  // pthread_join(idP, NULL);
 
-  for(int i=0; i<5; i++){
-    pthread_create(&idCP1[i], NULL, CP1, &i);
-    // if(i>0)
-    //   pthread_join(idCP1[i-1], NULL);
+  // Criando instâncias do CP1
+  for(int i=0; i<NCP1; i++){
+    pthread_create(&idCP1[i], NULL, CP1, (void *)i);
   }
 
-  for(int i=0; i<5; i++){
-    pthread_create(&idCP2[i], NULL, CP2, &i);
-    // if(i>0)
-    //   pthread_join(idCP2[i-1], NULL);
+  // Criando instâncias do CP2
+  for(int i=0; i<NCP2; i++){
+    pthread_create(&idCP2[i], NULL, CP2, (void *)i);
   }
 
-  for(int i=0; i<5; i++){
-    pthread_create(&idCP3[i], NULL, CP3, &i);
-    // if(i>0)
-    //   pthread_join(idCP3[i-1], NULL);
+  // Criando instâncias da CP3
+  for(int i=0; i<NCP3; i++){
+    pthread_create(&idCP3[i], NULL, CP3, (void *)i);
   }
 
+  // Criando instâncias do Consumidor
   pthread_create(&idC, NULL, Consumidor, 0);
 
-
-  // pthread_join(idC, NULL);
   pthread_exit(NULL);
 
   return 0;
